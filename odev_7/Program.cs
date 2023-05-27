c07475dfaa0fa9c316737512fae3105c4c1b8c72
Hp <Hp@DESKTOP-LTJOJD3>
2023-05-27 22:16:25 +0300

    Add project files.

---
+++ odev_7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int a = 1; a<=4; a++)
+            {
+                for (int b = 0; b < a; b++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
